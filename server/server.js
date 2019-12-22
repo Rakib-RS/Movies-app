f8de240a79d9425e4ee8aa46ecfa9de6f200067e
Rakib-RS <rakib.rscse@gmail.com>
2019-12-22 18:11:46 +0600

    preparing for deploy

--- server/server.js
+++ server/server.js
@@ -13,19 +13,23 @@ var port = process.env.PORT || 3300;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 /* /*Adds the react production build to serve react requests*/
-app.use(express.static(path.join(__dirname, "../client/build")));
+//app.use(express.static(path.join(__dirname, "../client/build")));
 /*React root
 app.get("*", (req, res) => {
 res.sendFile(path.join(__dirname + "../client/build/index.html"));
 });*/
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(logger('dev'));
 app.get('/',(req,res) =>{
     res.send('hello body');
 });
 app.use('/api',router);
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "../client/build/index.html"));
-    });
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+
 app.listen(port,(err)=>{
     if(err)
     return err;
