212bcf23c86c295cc3ab7467b329f7867cda86e3
Rakib-RS <rakib.rscse@gmail.com>
2019-12-22 16:24:53 +0600

    make it better

--- server/server.js
+++ server/server.js
@@ -5,19 +5,27 @@ const logger = require('morgan');
 var cors = require('cors');
 app.use(cors());
 const db = require('./db/data');
-
+const path = require('path');
 var router = require('./routes/movie-router');
 
 db.on('error',console.error.bind(console,'Mongodb Connection err'));
 var port = process.env.PORT || 3300;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-
+/* /*Adds the react production build to serve react requests*/
+app.use(express.static(path.join(__dirname, "../client/build")));
+/*React root
+app.get("*", (req, res) => {
+res.sendFile(path.join(__dirname + "../client/build/index.html"));
+});*/
 app.use(logger('dev'));
 app.get('/',(req,res) =>{
     res.send('hello body');
 });
 app.use('/api',router);
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "../client/build/index.html"));
+    });
 app.listen(port,(err)=>{
     if(err)
     return err;
