13ed19fdb810b54ff70e14f8e3cc69449912cd83
Rakib-RS <rakib.rscse@gmail.com>
2019-12-24 19:01:13 +0600

    modify

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const port = 3300;
+const port = process.env.PORT || 3300;
 const api = axios.create({
-    baseURL: 'http://122bf6a3.ngrok.io/api',
+    baseURL: 'http://localhost:'+port+'/api',
 })
 
 export const insertMovie = (payload) => api.post(`/movie`, payload)
